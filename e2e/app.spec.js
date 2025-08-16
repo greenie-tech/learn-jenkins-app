f00b28fb4ade512a77d4cf6bae8c0ad053aa8b00
Gtech <greenie.tech.llc@gmail.com>
2025-08-16 13:19:22 -0400

    added dynamic versioning fix-6

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,21 +2,21 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/', { timeout: 60000 });
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
 
 test('has Jenkins in the body', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/', { timeout: 60000 });
 
   const isVisible = await page.locator('a:has-text("Learn Jenkins on Udemy")').isVisible();
   expect(isVisible).toBeTruthy();
 });
 
 test('has expected app version', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/', { timeout: 60000 });
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
 
